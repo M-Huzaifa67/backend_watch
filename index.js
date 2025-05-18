1e075381452d5358ecd02cabed87b2ac44b852a6
M-Huzaifa67 <huzaifa.cs67@gmail.com>
2025-05-18 23:08:39 +0500

    set port

--- index.js
+++ index.js
@@ -2,40 +2,44 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const dontenv = require("dotenv");
+dontenv.config();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // This assumes you put the images inside /public/images/
-const localIP = "192.168.1.5"; // your PC's IP on LAN
-const port = 3000;
+// const localIP = "192.168.100.108"; 
+const localIP = "192.168.1.2";
+let port = process.env.PORT || 3000;
 
-const imagesUrl = [
-  `http://${localIP}:${port}/images/img1.jpg`,
-  `http://${localIP}:${port}/images/img2.jpg`,
-  `http://${localIP}:${port}/images/img3.jpg`,
-];
+
+// const imagesUrl = [
+//   `http://${localIP}:${port}/images/img1.jpg`,
+//   `http://${localIP}:${port}/images/img2.jpg`,
+//   `http://${localIP}:${port}/images/img3.jpg`,
+// ];
 
 const std_list = [
   {
     name: "Ali",
     age: 25,
-    imageUrl: imagesUrl[0],
+    // imageUrl: imagesUrl[0],
     city: "Lahore",
     country: "Pakistan",
   },
   {
     name: "Amir",
     age: 22,
-    imageUrl: imagesUrl[1],
+    // imageUrl: imagesUrl[1],
     city: "Karachi",
     country: "Pakistan",
   },
   {
     name: "Umar",
     age: 25,
-    imageUrl: imagesUrl[2],
+    // imageUrl: imagesUrl[2],
     city: "Lahore",
     country: "Pakistan",
   },
@@ -51,6 +55,6 @@ app.get("/data", (req, res) => {
 
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
